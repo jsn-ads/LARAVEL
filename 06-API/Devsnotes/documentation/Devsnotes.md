f1cb8549e76d3b97aa79c05861b4902a355e7fea
Neto <jsn.ads@gmail.com>
2021-05-25 23:44:10 -0300

    Criando o projeto de APi de anotaçoes

---
+++ 06-API/Devsnotes/documentation/Devsnotes.md
@@ -0,0 +1,24 @@
+Autor: José Alves de Souza Neto
+Data Inicio: 25/05/2021
+Data Conclusão : ?
+versao: 1.0
+
+TECNOLIGIAS:
+
+    FRONDEND
+
+    BACKEND
+        php
+        laravel
+SOBRE: 
+    criação de uma API de anotações
+
+FRONDEND:
+
+BACKEND:
+    Controllers:
+        NodeController
+    Models:
+        Node
+    Routes:
+        api.php
---
+++ 06-API/Devsnotes/routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/ping', function(Request $request){
+    return ['pong'=>true];
+});
+
+//listar
+Route::get('/notes', '');
+
+//edit
+Route::get('/note/{id}', '');
+
+//adicionar
+Route::post('/note', '');
+
+//atualizar edit
+Route::put('/note/{id}', '');
+
+//deletar
+Route::delete('/note/{id}','');
+
+
+
+

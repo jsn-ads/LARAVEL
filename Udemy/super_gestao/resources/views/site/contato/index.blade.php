5d6474eb651e80704d228b5f8fe3ca2e6bfe0313
Neto <jsn.ads@gmail.com>
2021-06-25 14:40:59 -0300

    criando template da views

--- Udemy/super_gestao/resources/views/site/contato/index.blade.php
+++ Udemy/super_gestao/resources/views/site/contato/index.blade.php
@@ -1,73 +1,51 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Contato</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    </head>
+@extends('site.template.basic')
 
-    <body>
-        <div class="topo">
-
-            <div class="logo">
-                <img src="{{ asset('assets/img/logo.png') }}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('inicio') }}">Principal</a></li>
-                    <li><a href="{{ route('sobre') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('contato') }}">Contato</a></li>
-                </ul>
-            </div>
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Entre em contato conosco</h1>
         </div>
 
-        <div class="conteudo-pagina">
-            <div class="titulo-pagina">
-                <h1>Entre em contato conosco</h1>
-            </div>
-
-            <div class="informacao-pagina">
-                <div class="contato-principal">
-                    <form>
-                        <input type="text" placeholder="Nome" class="borda-preta">
-                        <br>
-                        <input type="text" placeholder="Telefone" class="borda-preta">
-                        <br>
-                        <input type="text" placeholder="E-mail" class="borda-preta">
-                        <br>
-                        <select class="borda-preta">
-                            <option value="">Qual o motivo do contato?</option>
-                            <option value="">Dúvida</option>
-                            <option value="">Elogio</option>
-                            <option value="">Reclamação</option>
-                        </select>
-                        <br>
-                        <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                        <br>
-                        <button type="submit" class="borda-preta">ENVIAR</button>
-                    </form>
-                </div>
+        <div class="informacao-pagina">
+            <div class="contato-principal">
+                <form>
+                    <input type="text" placeholder="Nome" class="borda-preta">
+                    <br>
+                    <input type="text" placeholder="Telefone" class="borda-preta">
+                    <br>
+                    <input type="text" placeholder="E-mail" class="borda-preta">
+                    <br>
+                    <select class="borda-preta">
+                        <option value="">Qual o motivo do contato?</option>
+                        <option value="">Dúvida</option>
+                        <option value="">Elogio</option>
+                        <option value="">Reclamação</option>
+                    </select>
+                    <br>
+                    <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
+                    <br>
+                    <button type="submit" class="borda-preta">ENVIAR</button>
+                </form>
             </div>
         </div>
+    </div>
 
-        <div class="rodape">
-            <div class="redes-sociais">
-                <h2>Redes sociais</h2>
-                <img src="{{ asset('assets/img/facebook.png') }}">
-                <img src="{{ asset('assets/img/linkedin.png') }}">
-                <img src="{{ asset('assets/img/youtube.png')  }}">
-            </div>
-            <div class="area-contato">
-                <h2>Contato</h2>
-                <span>(11) 3333-4444</span>
-                <br>
-                <span>supergestao@dominio.com.br</span>
-            </div>
-            <div class="localizacao">
-                <h2>Localização</h2>
-                <img src="{{ asset('assets/img/mapa.png') }}">
-            </div>
+    <div class="rodape">
+        <div class="redes-sociais">
+            <h2>Redes sociais</h2>
+            <img src="{{ asset('assets/img/facebook.png') }}">
+            <img src="{{ asset('assets/img/linkedin.png') }}">
+            <img src="{{ asset('assets/img/youtube.png')  }}">
         </div>
-    </body>
-</html>
+        <div class="area-contato">
+            <h2>Contato</h2>
+            <span>(11) 3333-4444</span>
+            <br>
+            <span>supergestao@dominio.com.br</span>
+        </div>
+        <div class="localizacao">
+            <h2>Localização</h2>
+            <img src="{{ asset('assets/img/mapa.png') }}">
+        </div>
+    </div>
+@endsection
---
+++ Udemy/super_gestao/resources/views/site/template/basic.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    </head>
+
+    <body>
+
+        <div class="topo">
+            <div class="logo">
+                <img src="{{ asset('assets/img/logo.png') }}">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="{{ route('inicio') }}">Principal</a></li>
+                    <li><a href="{{ route('sobre') }}">Sobre</a></li>
+                    <li><a href="{{ route('contato') }}">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+
+        @yield('conteudo')
+    </body>
+</html>

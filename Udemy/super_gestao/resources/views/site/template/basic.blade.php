4ac3a16860c6e80be0e82cb4c0776528996734c5
Neto <jsn.ads@gmail.com>
2021-06-25 15:21:32 -0300

    include blade

--- Udemy/super_gestao/resources/views/site/template/basic.blade.php
+++ Udemy/super_gestao/resources/views/site/template/basic.blade.php
@@ -8,20 +8,7 @@
     </head>
 
     <body>
-
-        <div class="topo">
-            <div class="logo">
-                <img src="{{ asset('assets/img/logo.png') }}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('inicio') }}">Principal</a></li>
-                    <li><a href="{{ route('sobre') }}">Sobre</a></li>
-                    <li><a href="{{ route('contato') }}">Contato</a></li>
-                </ul>
-            </div>
-        </div>
+        @include('site.template.parcials.menu')
         @yield('conteudo')
     </body>
 </html>

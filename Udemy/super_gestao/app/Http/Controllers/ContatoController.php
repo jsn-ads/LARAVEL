591c2a96f7bf7e12b50f509fdd3cc0286533d37c
José <jsn.ads@gmail.com>
2021-09-28 14:06:50 -0300

    projeto super gestao formulario concluido

--- Udemy/super_gestao/app/Http/Controllers/ContatoController.php
+++ Udemy/super_gestao/app/Http/Controllers/ContatoController.php
@@ -22,14 +22,26 @@ class ContatoController extends Controller
     //funcao para adicionar contato
     public function add(Request $request){
 
+        $regras = [
+            'nome'                  => 'required|min:3|max:40',
+            'telefone'              => 'required|max:14',
+            'email'                 => 'required|max:100|email',
+            'id_motivo_contatos'    => 'required',
+            'mensagem'              => 'required|max:250'
+        ];
+
+        $msg = [
+            'required'      => 'o campo :attribute e obrigatorio',
+            'nome.min'      => 'o campo nome requer no minino 3 caracteres',
+            'nome.max'      => 'o campo nome permite no maximo 40 caracteres',
+            'telefone.max'  => 'o campo telefone permite no maximo 14 caracteres',
+            'email.max'     => 'o campo email permite no maximo 100 caracteres',
+            'email.email'   => 'email invalido',
+            'mensagem.max'  => 'o campo mensagem permit no maximo 250 caracteres'
+        ];
+
         //metodo validação
-        $request->validate([
-            'nome'              => 'required|min:3|max:40',
-            'telefone'          => 'required|max:14',
-            'email'             => 'required|max:100|email',
-            'id_motivo_contatos'=> 'required',
-            'mensagem'          => 'required|max:250'
-        ]);
+        $request->validate($regras , $msg);
 
         Contato::create($request->all());
 

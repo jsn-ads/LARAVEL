e77cb544c68bb4be8362722bf2e9619cffd37952
Neto <jsn.ads@gmail.com>
2021-09-29 00:00:17 -0300

    projeto super gestao middleware

--- Udemy/super_gestao/app/Http/Middleware/LogAcessoMiddleware.php
+++ Udemy/super_gestao/app/Http/Middleware/LogAcessoMiddleware.php
@@ -3,8 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
+
 use Facade\FlareClient\Http\Response;
+
 use Illuminate\Http\Request;
+
+use App\Models\LogAcesso;
 
 class LogAcessoMiddleware
 {
@@ -17,6 +21,15 @@ class LogAcessoMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+
+        LogAcesso::create([
+            'ip' => $ip,
+            'rota' => $rota
+        ]);
+
         // request recebe a requisição do navegador e manipula antes de chegar no servidor
         return $next($request);
         // retorna a resposa do servidor

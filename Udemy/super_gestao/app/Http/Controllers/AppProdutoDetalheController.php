cdac6702e34bf1ef538700492559afdad195bc39
José <jsn.ads@gmail.com>
2021-10-08 17:33:47 -0300

    projeto super gestao Elonquet ORM 1 a1

--- Udemy/super_gestao/app/Http/Controllers/AppProdutoDetalheController.php
+++ Udemy/super_gestao/app/Http/Controllers/AppProdutoDetalheController.php
@@ -3,24 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Models\ProdutoDetalhe;
+use App\Models\Unidade;
+
 use Illuminate\Http\Request;
 
 class AppProdutoDetalheController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        return "cheguei";
+
+        $produto_detalhes = ProdutoDetalhe::paginate(10);
+
+        return view('app.produto_detalhe.index',[
+            'produto_detalhes' => $produto_detalhes,
+            'request' => $request->all()
+        ]);
+
     }
 
     public function create()
     {
-        //
+        $unidades = Unidade::all();
+        return view('app.produto_detalhe.create',[
+            'unidades' => $unidades
+        ]);
     }
 
     public function store(Request $request)
     {
-        //
+        ProdutoDetalhe::create($request->all());
+
+        return redirect()->route('produto_detalhe.index');
     }
 
     public function show(ProdutoDetalhe $produtoDetalhe)
@@ -30,7 +44,13 @@ class AppProdutoDetalheController extends Controller
 
     public function edit(ProdutoDetalhe $produtoDetalhe)
     {
-        //
+
+        $unidades = Unidade::all();
+
+        return view('app.produto_detalhe.edit',[
+            'produto_detalhe' => $produtoDetalhe,
+            'unidades'=> $unidades
+        ]);
     }
 
     public function update(Request $request, ProdutoDetalhe $produtoDetalhe)
--- Udemy/super_gestao/app/Models/ProdutoDetalhe.php
+++ Udemy/super_gestao/app/Models/ProdutoDetalhe.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ProdutoDetalhe extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id_produto','largura','altura','comprimento','id_unidade'];
 }

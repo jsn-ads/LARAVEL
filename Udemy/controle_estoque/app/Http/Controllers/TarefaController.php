25ee78768bb5e06c91414ceeab4e2210ae63275e
JOSE EVTEK <jsn.ads@gmail.com>
2021-10-26 10:35:59 -0300

    projeto controle_estoque: configurando atenticação no controller tarefa

--- Udemy/controle_estoque/app/Http/Controllers/TarefaController.php
+++ Udemy/controle_estoque/app/Http/Controllers/TarefaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TarefaController extends Controller
 {
@@ -14,7 +15,11 @@ class TarefaController extends Controller
      */
     public function index()
     {
-        echo 'tarefas';
+         $id = Auth::user()->id;
+         $name = Auth::user()->name;
+         $email = Auth::user()->email;
+
+         return "ID: $id | Nome: $name | E-mail: $email";
     }
 
     /**

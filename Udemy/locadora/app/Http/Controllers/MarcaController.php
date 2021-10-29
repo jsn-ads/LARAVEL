9d473cbe4e171a0463b104464836502a7daacf3f
JOSE EVTEK <jsn.ads@gmail.com>
2021-10-29 11:36:10 -0300

    projeto Locadora API Rest: validando metodos edição e exclusão

--- Udemy/locadora/app/Http/Controllers/MarcaController.php
+++ Udemy/locadora/app/Http/Controllers/MarcaController.php
@@ -13,34 +13,16 @@ class MarcaController extends Controller
         $this->marca = $marca;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Request $request)
     {
-        // $marcas = Marca::all();
         return $this->marca->all();
-
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->marca->nome =   $request->input('nome');
@@ -48,53 +30,23 @@ class MarcaController extends Controller
         return $this->marca->save();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        return $this->marca->find($id);
+        return ($this->marca->find($id) != null) ? $this->marca->find($id) : ['erro'=>'Marca não encontrada'];
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Marca $marca)
     {
 
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-
-        $this->marca->find($id)->update($request->all());
-
-        return "Atualizado com sucesso";
+        return ($this->marca->find($id) != null) ? $this->marca->find($id)->update($request->all()) : ['erro'=>'Erro ao Atualizar , Marca não encontrada'];
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $this->marca->find($id)->delete();
-
-        return ['msg' => 'a marca foi excluida com sucesso'];
+        return ($this->marca->find($id) != null) ? $this->marca->find($id)->delete() : ['erro'=>'Erro ao Deletar , Marca não encontrada'];
     }
 }

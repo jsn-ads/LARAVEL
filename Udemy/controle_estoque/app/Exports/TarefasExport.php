3b7fa383f58439bbcd2f1a220efb73e2058178f5
Neto <jsn.ads@gmail.com>
2021-12-14 23:38:30 -0300

    projeto controle de estoque : configurando titulo na exportação

--- Udemy/controle_estoque/app/Exports/TarefasExport.php
+++ Udemy/controle_estoque/app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,11 @@ class TarefasExport implements FromCollection
     public function collection()
     {
         return auth()->user()->tarefas()->get();
+    }
+
+    //metodo incluir titulo na exportação
+
+    public function headings():array{
+        return ['id','usuario','tarefa','data conclusão','data criação', 'data atualização'];
     }
 }

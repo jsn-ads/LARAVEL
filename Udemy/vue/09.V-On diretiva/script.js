c2374b93d32d3e6d6032794ca04595c748492999
Jose Evtek <jsn.ads@gmail.com>
2022-01-04 11:49:24 -0300

    aprendizado vue : implementando modificadores

--- Udemy/vue/09.V-On diretiva/script.js
+++ Udemy/vue/09.V-On diretiva/script.js
@@ -1,7 +1,21 @@
 const vm = new Vue({
     el:"#app",
     data() {
-        return {}
+        return {
+            d1: 'esperando...',
+            d2: 0,
+            d3: ""
+        }
     },
-    methods: {},
+    methods: {
+        metodo1(){
+            this.d1 = 'bloquendo o envio'
+        },
+        metodo2(){
+            this.d2++
+        },
+        metodo3(e){
+            this.d3 += e.key;
+        }
+    },
 })

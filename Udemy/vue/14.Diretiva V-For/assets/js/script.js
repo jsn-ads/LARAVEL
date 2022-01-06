b4b72201a0472bf2bace210957ac4390c7d00880
Neto <jsn.ads@gmail.com>
2022-01-05 21:36:54 -0300

    aprendizado vue : diretiva v-for ...

--- Udemy/vue/14.Diretiva V-For/assets/js/script.js
+++ Udemy/vue/14.Diretiva V-For/assets/js/script.js
@@ -2,7 +2,23 @@ const vm = new Vue({
     el:"#app",
     data() {
         return {
-            lista : ['laravel','vue','mysql','bootstrap']
+            lista  : [
+                'laravel',
+                'vue',
+                'mysql',
+                'bootstrap'
+            ],
+
+            cursos : [
+                 {
+                    nome: "laravel",
+                    descricao : "framework de php"
+                },
+                {
+                    nome: "vue",
+                    descricao : "framework de javascript"
+                }
+            ],
         }
     },
     methods: {

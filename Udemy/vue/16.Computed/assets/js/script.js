ea82b1e8f1c0823291255960ddc57dcc8b951065
Jose Evtek <jsn.ads@gmail.com>
2022-01-07 12:03:50 -0300

    aprendizado vue : computed ...

--- Udemy/vue/16.Computed/assets/js/script.js
+++ Udemy/vue/16.Computed/assets/js/script.js
@@ -16,4 +16,23 @@ const vm = new Vue({
            console.log(this.pacientes)
         }
     },
+    computed : {
+        ultimoPaciente(){
+
+            let key =  this.pacientes.lenght -1
+
+            let paciente = array()
+
+            paciente = {
+                'nome' : this.pacientes[key].nome ,
+                'idade': this.pacientes[key].idade,
+                'plano': this.pacientes[key].plano,
+            }
+
+            console.log(paciente)
+
+            return paciente
+
+        }
+    }
 })

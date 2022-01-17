fe28e6bc1bc1dc885aecdab18271082a5290cdf9
Jose Evtek <jsn.ads@gmail.com>
2022-01-17 17:28:35 -0300

    projeto locadora API Rest : codificando modelo

--- Udemy/locadora/app/Models/Modelo.php
+++ Udemy/locadora/app/Models/Modelo.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id_marca','imagem','np','lugares','air_bag','abs'];
+
+    // regras de validação
+    public function rules(){
+        return [
+            'nome'    => 'required|unique:marcas,nome,'.$this->id.'|min : 3',
+            'imagem'  => 'required|file|mimes:png,jpeg,jpg',
+            'np'      => 'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,5',
+            'air_bag' => 'required|boolean',
+            'abs'     => 'required|boolean'
+        ];
+    }
+
+    // reposta de retorno
+    public function feedback(){
+        return [
+            'required'        => 'o campo :attribute e obrigatório',
+            'nome.unique'     => 'este nome ja esta sendo utilizado',
+            'imagem.mimes'    => 'erro ao salvar arquivo , somente arquivo png',
+            'digits_between'  => 'numero de :attribute e invalido'
+        ];
+    }
 }

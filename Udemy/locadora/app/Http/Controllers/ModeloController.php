d8aa4da4db59b63bfe36dd2585fd54a32e4bf038
Jose Evtek <jsn.ads@gmail.com>
2022-01-18 15:28:23 -0300

    projeto locadora API Rest : codificando modelo

--- Udemy/locadora/app/Http/Controllers/ModeloController.php
+++ Udemy/locadora/app/Http/Controllers/ModeloController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
 
 class ModeloController extends Controller
 {
@@ -11,7 +13,8 @@ class ModeloController extends Controller
 
     public function __construct(Modelo $modelo)
     {
-
+        $this->dados = array();
+        $this->modelo = $modelo;
     }
 
     /**
@@ -19,9 +22,9 @@ class ModeloController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        return response()->json($this->modelo->all(),200);
     }
 
     /**
@@ -42,7 +45,18 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules(), $this->modelo->feedback());
+
+        $this->modelo->id      =  $request->input('id_marca');
+        $this->modelo->imagem  =  $request->file('imagem');
+        $this->modelo->imagem  =  $this->modelo->imagem->store('imagens/modelo','public');
+        $this->modelo->np      =  $request->input('np');
+        $this->modelo->lugares =  $request->input('lugares');
+        $this->modelo->air_bag =  $request->input('air_bag');
+        $this->modelo->abs     =  $request->input('abs');
+
+        return response()->json($this->produto->save(),200);
+
     }
 
     /**
@@ -51,9 +65,9 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        return ($this->modelo->find($id) != null) ? response()->json($this->modelo->find($id),200) : response()->json(['erro' => 'Modelo não encontrado'], 404);
     }
 
     /**
@@ -74,9 +88,28 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+
+        $request->validate($this->modelo->rules(), $this->modelo->feedback());
+
+        $this->dados['id_marca'] =  $request->input('id_marca');
+        $imagem  =  $request->file('imagem');
+        $this->dados['imagem']   =  $imagem->store('imagens/modelo','public');
+        $this->dados['np']       =  $request->input('np');
+        $this->dados['lugares']  =  $request->input('lugares');
+        $this->dados['air_bag']  =  $request->input('air_bag');
+        $this->dados['abs']      =  $request->input('abs');
+
+        if($request->file('imagem')){
+
+            $this->modelo = $this->modelo->find($id);
+
+            Storage::disk('public')->delete($this->modelo->imagem);
+        }
+
+        return response()->json($this->modelo->find($id)->update($this->dados),200);
+
     }
 
     /**
@@ -85,8 +118,16 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        if($this->marca->find($id)){
+
+            $this->marca = $this->marca->find($id);
+
+            Storage::disk('public')->delete($this->marca->imagem);
+
+        }
+
+        return ($this->marca->find($id) != null) ? response()->json($this->marca->find($id)->delete(),200) : response()->json(['erro'=>'Erro ao Deletar , Marca não encontrada'],404);
     }
 }

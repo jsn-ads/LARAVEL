0f0ad44783f0d4b08454af4c7456069e9852aa33
Jose Evtek <jsn.ads@gmail.com>
2022-02-02 11:01:00 -0300

    projeto locadora API Rest : criando locacao repository e desenvolvendo locacao e controller

--- Udemy/locadora/app/Http/Controllers/LocacaoController.php
+++ Udemy/locadora/app/Http/Controllers/LocacaoController.php
@@ -3,83 +3,101 @@
 namespace App\Http\Controllers;
 
 use App\Models\Locacao;
+use App\Repositories\LocacaoRepository;
 use Illuminate\Http\Request;
 
 class LocacaoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct(Locacao $locacao)
     {
-        //
+        $this->locacao = $locacao;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function index(Request $request)
+    {
+        $locacaoRepository = new LocacaoRepository($this->locacao);
+
+        if($request->has('filtro'))
+        {
+            $locacaoRepository->filtro($request->filtro);
+        }
+
+        if($request->has('atributos'))
+        {
+            $locacaoRepository->atributos($request->atributos);
+        }
+
+        return response()->json($locacaoRepository->get(),200);
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->locacao->rules(), $this->locacao->feedback());
+
+        $this->locacao->id_cliente     =  $request->input('id_cliente');
+        $this->locacao->id_carro       =  $request->input('id_carro');
+        $this->locacao->data_inicio    =  $request->input('data_inicio');
+        $this->locacao->data_previsto  =  $request->input('data_previsto');
+        $this->locacao->data_final     =  $request->input('data_final');
+        $this->locacao->valor_diaria   =  $request->input('valor_diaria');
+        $this->locacao->km_inicial     =  $request->input('km_inicial');
+        $this->locacao->km_final       =  $request->input('km_final');
+
+        return response()->json($this->locacao->save(),200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Locacao $locacao)
+    public function show($id)
     {
-        //
+        return ($this->locacao->find($id) != null) ? response()->json($this->locacao->find($id),200) : response()->json(['erro' => 'locacao não encontrado'], 404);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Locacao $locacao)
+    public function edit($id)
     {
-        //
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Locacao $locacao)
+    public function update(Request $request, $id)
     {
-        //
+        if($request->method() === "PATCH")
+        {
+
+            $regrasDinamicas = array();
+
+            foreach($this->locacao->rules() as $input => $regra)
+            {
+
+                if(array_key_exists($input, $request->all()))
+                {
+
+                    $regrasDinamicas[$input] = $regra;
+
+                }
+            }
+
+            $request->validate($regrasDinamicas, $this->locacao->find($id)->feedback());
+
+        }
+        else
+        {
+
+            $request->validate($this->locacao->rules(), $this->locacao->feedback());
+
+        }
+
+        $this->locacao = $this->locacao->find($id);
+
+        $this->locacao->fill($request->all());
+
+        return response()->json($this->locacao->save(),200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Locacao $locacao)
+    public function destroy($id)
     {
-        //
+        return ($this->locacao->find($id) != null) ? response()->json($this->locacao->find($id)->delete(),200) : response()->json(['erro'=>'Erro ao Deletar , Locacao não encontrado'],404);
     }
 }
--- Udemy/locadora/app/Models/Locacao.php
+++ Udemy/locadora/app/Models/Locacao.php
@@ -10,4 +10,42 @@ class Locacao extends Model
     use HasFactory;
 
     protected $table = 'locacoes';
+
+    protected $fillable =
+    [
+        'id_cliente',
+        'id_carro',
+        'data_inicio',
+        'data_previsto',
+        'data_final',
+        'valor_diaria',
+        'km_inicial',
+        'km_final'
+    ];
+
+    public function rules()
+    {
+        return
+        [
+            'id_cliente'    => 'required',
+            'id_carro'      => 'required',
+            'data_inicio'    => 'required|date',
+            'data_previsto' => 'date',
+            'data_final'    => 'date',
+            'valor_diaria'  => 'required',
+            'km_inicial'    => 'required|integer',
+            'km_final'      => 'required|integer'
+
+        ];
+    }
+
+    public function feedback()
+    {
+        return
+        [
+            'required' => 'o campo :attribute e obrigatorio',
+            'date' => 'Informe uma data valida',
+            'integer' => "Informe apenas Numeros"
+        ];
+    }
 }

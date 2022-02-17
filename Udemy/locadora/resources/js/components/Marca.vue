1b7a01fd87cae5ce2559ffeaa0d573442e7f63e6
Jose Evtek <jsn.ads@gmail.com>
2022-02-17 08:23:47 -0300

    projeto locadora API Rest : criando tabela marca

--- Udemy/locadora/resources/js/components/Marca.vue
+++ Udemy/locadora/resources/js/components/Marca.vue
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+
+                <!-- card pesquisa ini -->
+                <div class="card mb-3">
                     <div class="card-header">Marcas</div>
 
                     <div class="card-body">
@@ -24,6 +26,50 @@
                          <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
                     </div>
                 </div>
+                <!-- card pesquisa end -->
+
+                <!-- card tabela ini -->
+                <div class="card">
+                    <div class="card-header">Tabela de Marcas</div>
+
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                         </table>
+                    </div>
+
+                    <div class="card-footer">
+                         <button type="submit" class="btn btn-primary btn-sm float-right">Adicionar</button>
+                    </div>
+                </div>
+                <!-- card tabela end -->
+
             </div>
         </div>
     </div>
